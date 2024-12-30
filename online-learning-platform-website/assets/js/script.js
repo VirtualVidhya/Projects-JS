69a03590f1f1d25570c8e0f049b982127986d075
kharva <parvadesai404@gmail.com>
2024-12-31 05:15:47 +0530

    [Fix] refactoring

--- online-learning-platform-website/assets/js/script.js
+++ online-learning-platform-website/assets/js/script.js
@@ -1,10 +1,6 @@
-'use strict';
+"use strict";
 
-
-
-/**
- * add event on element
- */
+// add event on element
 
 const addEventOnElem = function (elem, type, callback) {
   if (elem.length > 1) {
@@ -14,13 +10,9 @@ const addEventOnElem = function (elem, type, callback) {
   } else {
     elem.addEventListener(type, callback);
   }
-}
+};
 
-
-
-/**
- * navbar toggle
- */
+// navbar toggle
 
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
@@ -30,22 +22,18 @@ const overlay = document.querySelector("[data-overlay]");
 const toggleNavbar = function () {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
-}
+};
 
 addEventOnElem(navTogglers, "click", toggleNavbar);
 
 const closeNavbar = function () {
   navbar.classList.remove("active");
   overlay.classList.remove("active");
-}
+};
 
 addEventOnElem(navLinks, "click", closeNavbar);
 
-
-
-/**
- * header active when scroll down to 100px
- */
+// header active when scroll down to 100px
 
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
@@ -58,6 +46,6 @@ const activeElem = function () {
     header.classList.remove("active");
     backTopBtn.classList.remove("active");
   }
-}
+};
 
 addEventOnElem(window, "scroll", activeElem);
