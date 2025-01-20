fb89658149a8d44e3fb4f514bfb22160ee58e211
kharva <parvadesai404@gmail.com>
2025-01-21 00:06:01 +0530

    [Add] clothing-store site front-page

---
+++ clothing-store-website/landing-page-products-script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const productsContainer = document.getElementById("product-grid");
+
+  fetch("landing-page-products.-data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const productsHTML = data.products
+        .map(
+          (product) => `
+                <a href="#" class="product-card">
+                    <div class="product-img-holder">
+                        <div class="product-img"></div>
+                    </div>
+                    <p class="product-name">
+                        ${product.name}
+                    </p>
+                    <p class="product-desc">${product.description}</p>
+                    <p class="product-price">rs. ${product.price}</p>
+                </a>
+            `
+        )
+        .join("");
+
+      productsContainer.innerHTML = productsHTML;
+
+      const productImages = document.querySelectorAll(".product-img");
+      productImages.forEach((img, index) => {
+        img.style.backgroundImage = `url('${data.products[index].image}')`;
+      });
+    });
+});
